e004cbc926bb5fba611ff5283c2859dc847796bc
xdwu <xdwu@cn.ibm.com>
2019-07-01 13:29:02 +0800

    cancel generate report page

--- src/test/java/io/cucumber/samples/dw/HelloCucumberTest.java
+++ src/test/java/io/cucumber/samples/dw/HelloCucumberTest.java
@@ -16,22 +16,22 @@ public class HelloCucumberTest {
 
     @Test
     public void generateDemoReport() throws IOException {
-        File reportOutputDirectory = new File("target/demo");
-        List<String> jsonFiles = new ArrayList<>();
-        jsonFiles.add("target/json-report/do.json");
-
-        String buildNumber = "1";
-        String projectName = "dw";
-        Configuration configuration = new Configuration(reportOutputDirectory, projectName);
-        configuration.setBuildNumber(buildNumber);
-
-        configuration.addClassifications("Browser", "Firefox");
-        configuration.addClassifications("Master", "release/1.0");
-        configuration.setSortingMethod(SortingMethod.NATURAL);
-        configuration.addPresentationModes(PresentationMode.EXPAND_ALL_STEPS);
-        configuration.setTrendsStatsFile(new File("target/json-report/demo"));
-
-        ReportBuilder reportBuilder = new ReportBuilder(jsonFiles, configuration);
-        reportBuilder.generateReports();
+//        File reportOutputDirectory = new File("target/demo");
+//        List<String> jsonFiles = new ArrayList<>();
+//        jsonFiles.add("target/json-report/do.json");
+//
+//        String buildNumber = "1";
+//        String projectName = "dw";
+//        Configuration configuration = new Configuration(reportOutputDirectory, projectName);
+//        configuration.setBuildNumber(buildNumber);
+//
+//        configuration.addClassifications("Browser", "Firefox");
+//        configuration.addClassifications("Master", "release/1.0");
+//        configuration.setSortingMethod(SortingMethod.NATURAL);
+//        configuration.addPresentationModes(PresentationMode.EXPAND_ALL_STEPS);
+//        configuration.setTrendsStatsFile(new File("target/json-report/demo"));
+//
+//        ReportBuilder reportBuilder = new ReportBuilder(jsonFiles, configuration);
+//        reportBuilder.generateReports();
     }
 }
